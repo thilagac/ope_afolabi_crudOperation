425a2a4cb251fdb725b9f208e6b1019b255488c7
karthika <dinakarankarthi64@gmail.com>
2024-02-29 15:57:02 +0530

    first commit

---
+++ src/component/EditToDoForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditToDoForm = () => {
+  return (
+    <div>EditToDoForm</div>
+  )
+}
+
+export default EditToDoForm
---
+++ src/component/ToDoWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDoForm from './ToDoForm'
+
+function ToDoWrapper() {
+  return (
+    <div className='TodoWrapper'>
+      <ToDoForm/>
+    </div>
+  )
+}
+
+export default ToDoWrapper
